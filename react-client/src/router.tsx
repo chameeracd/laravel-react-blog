58c7399b0246eba97b050cd800aa4a9aeb615a12
Chameera Dilhan <chameeracd@gmail.com>
2024-09-23 22:10:17 +0530

    auth + user views

---
+++ react-client/src/router.tsx
@@ -0,0 +1,45 @@
+import { createBrowserRouter } from "react-router-dom";
+import Login from "./views/auth/login.tsx";
+import Register from "./views/auth/register.tsx";
+import DefaultLayout from "./components/defaultLayout.tsx";
+import GuestLayout from "./components/guestLayout.tsx";
+import Users from "./views/user/users.tsx";
+import UserForm from "./views/user/userForm.tsx";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <DefaultLayout />,
+        children: [
+            {
+                path: "/users",
+                element: <Users />,
+            },
+            {
+                path: "/users/new",
+                element: <UserForm key="userCreate" />,
+            },
+            {
+                path: "/users/:id",
+                element: <UserForm key="userUpdate" />,
+            },
+        ],
+    },
+
+    {
+        path: "/",
+        element: <GuestLayout />,
+        children: [
+            {
+                path: "/login",
+                element: <Login />,
+            },
+            {
+                path: "/register",
+                element: <Register />,
+            },
+        ],
+    },
+]);
+
+export default router;
